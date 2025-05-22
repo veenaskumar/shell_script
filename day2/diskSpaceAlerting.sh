4b3607d16f248fb562c5d189589405c604cc1d3d
veenaskumar <venaskumar812@gmail.com>
2025-05-22 20:09:37 +0530

    updated the logic and table alignment

--- day2/diskSpaceAlerting.sh
+++ day2/diskSpaceAlerting.sh
@@ -1,3 +1,20 @@
 #! /bin/bash
 
+#!/bin/bash
+
 echo "Welcome to disk usage alert system"
+
+
+if df -h | awk '{gsub("%","",$5)} $5 > 80' | grep -q .; then
+  echo -e "\nWarning: High disk usage detected!\n"
+  df -h | awk '
+    NR > 1 {
+      gsub("%", "", $5);
+      if ($5 > 80)
+        printf "%-s %-s %-s\n", $6, $5"%", $2
+    }
+  ' | column -t -N "MountPoint","Usage","Size"
+else
+  echo "All disk usage is under control."
+fi
+
